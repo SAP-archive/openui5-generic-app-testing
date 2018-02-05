dfc11b6560b3983e9c395cacea5a2d8b16eebe96
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2018-02-05 22:09:53 +0000

    [FIX] Change module generation

--- webapp/src/steps/iNavigateTo.js
+++ webapp/src/steps/iNavigateTo.js
@@ -1,39 +1,30 @@
-/*! openui5-generic-app-testing 2017-11-22 */
+module.exports = {
+	docs: {
+		description: "Performs navigation using hash",
+		synopsis: "I navigate to <hash> [with <object>]",
+		examples: [
+			"I navigate to /Start/GoodsReceipts",
+			"I navigate to /Start/GoodsReceipts with {'OrderNumber': '4711'}"
+		]
+	},
+	regexp: new RegExp(["^I navigate to\\s+", "(\/[a-zA-Z0-9\/]+)", "(\\s+with\\s+(.+?)\\s)?$"].join("")),
+	icon: "cursor-arrow",
+	action: function(sId, sHash, sObject) {
+		var oWaitForOptions, oObject, that, oHasherChanger;
+		that = this;
 
-(function() {
-	var module = {};
-	module.exports = {
-		docs: {
-			description: "Performs navigation using hash",
-			synopsis: "I navigate to <hash> [with <object>]",
-			examples: [
-				"I navigate to /Start/GoodsReceipts",
-				"I navigate to /Start/GoodsReceipts with {'OrderNumber': '4711'}"
-			]
-		},
-		regexp: new RegExp(["^I navigate to\\s+", "(\/[a-zA-Z0-9\/]+)", "(\\s+with\\s+(.+?)\\s)?$"].join("")),
-		icon: "cursor-arrow",
-		action: function(sId, sHash, sObject) {
-			var oWaitForOptions, oObject, that, oHasherChanger;
-			that = this;
+		oObject = JSON.parse(sObject);
 
-			oObject = JSON.parse(sObject);
+		oHasherChanger = sap.ui.test.Opa5.getHashChanger();
 
-			oHasherChanger = sap.ui.test.Opa5.getHashChanger();
+		oHasherChanger.setHash(sHash);
 
-			oHasherChanger.setHash(sHash);
+		oWaitForOptions = {
+			success: function() {
+				that.Opa5.assert.strictEquals(oHasherChanger.getHash(), sHash, "Hash '" + sHash + "' set successfully.");
+			}
+		};
+		that.opa.waitFor(oWaitForOptions);
 
-			oWaitForOptions = {
-				success: function() {
-					that.Opa5.assert.strictEquals(oHasherChanger.getHash(), sHash, "Hash '" + sHash + "' set successfully.");
-				}
-			};
-			that.opa.waitFor(oWaitForOptions);
-
-		}
-	};
-	module.exports.name = "iNavigateTo";
-	sap.ui.define([], function() {
-		return module.exports;
-	});
-})();
+	}
+};
--- webapp/src/steps/iSetFocusOn.js
+++ webapp/src/steps/iSetFocusOn.js
@@ -1,35 +1,26 @@
-/*! openui5-generic-app-testing 2017-11-22 */
+module.exports = {
+	docs: {
+		description: "Set focus on a control",
+		synopsis: "I set focus on '<controlId>' [in <viewName> view]",
+		examples: ["I set focus on 'inputField'", "I set focus on 'saveButton' in Creation view"]
+	},
+	icon: "edit",
+	regexp: /^I set focus on (.+?)( in ([a-zA-Z0-9\.]+) view)?$/,
+	action: function(sControlId, sViewPart, sViewName) {
+		var that = this;
+		var oWaitForOptions = {
+			id: sControlId,
+			success: function(oControl) {
 
-(function() {
-	var module = {};
-	module.exports = {
-		docs: {
-			description: "Set focus on a control",
-			synopsis: "I set focus on '<controlId>' [in <viewName> view]",
-			examples: ["I set focus on 'inputField'", "I set focus on 'saveButton' in Creation view"]
-		},
-		icon: "edit",
-		regexp: /^I set focus on (.+?)( in ([a-zA-Z0-9\.]+) view)?$/,
-		action: function(sControlId, sViewPart, sViewName) {
-			var that = this;
-			var oWaitForOptions = {
-				id: sControlId,
-				success: function(oControl) {
-
-					//var ojQuery = sap.ui.test.Opa5.getJQuery();
-					//var oDomRef = ojQuery.sap.domById(oControl.getId());
-					//ojQuery.sap.focus(oDomRef);
-					oControl.focus();
-				}
-			};
-			if (sViewPart) {
-				oWaitForOptions.viewName = sViewName;
+				//var ojQuery = sap.ui.test.Opa5.getJQuery();
+				//var oDomRef = ojQuery.sap.domById(oControl.getId());
+				//ojQuery.sap.focus(oDomRef);
+				oControl.focus();
 			}
-			this.opa.waitFor(oWaitForOptions);
+		};
+		if (sViewPart) {
+			oWaitForOptions.viewName = sViewName;
 		}
-	};
-	module.exports.name = "iSetFocusOn";
-	sap.ui.define([], function() {
-		return module.exports;
-	});
-})();
+		this.opa.waitFor(oWaitForOptions);
+	}
+};
