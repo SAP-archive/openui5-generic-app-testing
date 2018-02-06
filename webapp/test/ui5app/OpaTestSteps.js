92f289b47f0a49ef89dcbe3f76e4e2091f9a1695
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2018-02-06 20:21:12 +0000

    [ADD] Test for iCanSee

--- webapp/test/ui5app/OpaTestSteps.js
+++ webapp/test/ui5app/OpaTestSteps.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "jquery.sap.global",
     "ui5app/utils/GenericSteps", // ui5app.utils mapped via sap-ui-resourceroots
     "sap/ui/test/gherkin/StepDefinitions",
-    "sap/ui/test/Opa5",
+    "sap/ui/test/Opa5"
 ], function($, oGenericSteps, StepDefinitions, Opa5) {
 
     return StepDefinitions.extend("ui5app/test/Steps", {
